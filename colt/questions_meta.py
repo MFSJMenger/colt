fb0b6668e2d5047849b1cbf7c2dfbfacddc55b78
Maximilian Menger <menger.maximilian@gmail.com>
2019-11-14 01:23:51 +0100

    fixed bug in metaclass

--- colt/questions_meta.py
+++ colt/questions_meta.py
@@ -2,16 +2,25 @@ from abc import ABCMeta, abstractmethod
 #
 from .generator import QuestionGenerator
 
+
 class _QuestionsHandlerMeta(ABCMeta):
 
     @property
-    def questions(cls):
+    def _generate_questions(cls):
         questions = QuestionGenerator(cls._questions)
         cls._generate_subquestion(questions)
         return questions.questions
+
+    @property
+    def questions(cls):
+        return cls._generate_questions
         
     def _generate_subquestion(cls, questions):
         pass
 
+
 class Colt(metaclass=_QuestionsHandlerMeta):
-    pass
+    
+    @classmethod
+    def questions(cls):
+        return cls._generate_questions
