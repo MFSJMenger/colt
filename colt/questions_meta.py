6473435e4d4e6ef87a6af98689de85c9652de9bd
Maximilian Menger <menger.maximilian@gmail.com>
2019-11-14 03:46:58 +0100

    Fixed problem with instances and classes

--- colt/questions_meta.py
+++ colt/questions_meta.py
@@ -4,23 +4,26 @@ from .generator import QuestionGenerator
 
 
 class _QuestionsHandlerMeta(ABCMeta):
+    """Metaclass to handle hierarchical generation of questions"""
 
     @property
     def _generate_questions(cls):
+        """generate questions"""
         questions = QuestionGenerator(cls._questions)
         cls._generate_subquestions(questions)
         return questions.questions
 
     @property
     def questions(cls):
-        return cls._generate_questions
+        return cls._generate_questions()
         
     def _generate_subquestions(cls, questions):
         pass
 
 
 class Colt(metaclass=_QuestionsHandlerMeta):
+    """Basic Class to manage colts question routines"""
     
-    @classmethod
-    def get_questions(cls):
-        return cls._generate_questions
+    @property
+    def questions(self):
+        return self.__class__.questions
