d88306fa71bea98c675d1cc0867765bd262bdec2
Maximilian Menger <menger.maximilian@gmail.com>
2019-11-21 13:24:19 +0100

    added subquestions annotations

--- colt/colt.py
+++ colt/colt.py
@@ -23,14 +23,45 @@ def delete_inherited_keys(keys, clsdict):
         if clsdict[key] == 'inherited':
             del clsdict[key]
 
+def join_subquestions(func1, func2):
+    if isinstance(func1, classmethod):
+        func1 = func1.__func__
+    if isinstance(func2, classmethod):
+        func2 = func2.__func__
 
-def colt_meta_setup(clsdict):
-    """setup the clsdict in colt to avoid inheritance problems"""
+
+    def _generate_subquestions(cls, questions):
+        func1(questions)
+        func2(cls, questions)
+
+    return classmethod(_generate_subquestions)
+
+
+def colt_modify_class_dict(clsdict, bases):
+    """setup the clsdict in colt to avoid inheritance problems
+    
+       it modifies both the clsdict and its annotations! 
+    """
     colt_defaults = {
             '_generate_subquestions': classmethod(lambda cls, questions: 0),
             '_questions': "",
     }
-    
+    if '__annotations__' in clsdict:
+        print(clsdict)
+    # rewrite that....it is horrible
+    if clsdict.get('__annotations__', None) is not None:
+        if clsdict['__annotations__'].get('subquestions', None) == 'inherited':
+            if '_generate_subquestions' in clsdict:
+                print(clsdict['_generate_subquestions'])
+                if len(bases) > 0:
+                    clsdict['_generate_subquestions'] = join_subquestions(bases[0]._generate_subquestions, clsdict['_generate_subquestions'])
+            else: 
+                clsdict['_generate_subquestions'] = bases[0]._generate_subquestions
+            # delete task from annotations, and clean unnecessary annotations!    
+            del clsdict['__annotations__']['subquestions'] 
+            if clsdict['__annotations__'] == {}:
+                del clsdict['__annotations__']
+    #
     add_defaults_to_dict(clsdict, colt_defaults)
     delete_inherited_keys(["_questions"], clsdict)
 
@@ -39,7 +70,7 @@ class ColtMeta(ABCMeta):
     """Metaclass to handle hierarchical generation of questions"""
 
     def __new__(cls, name, bases, clsdict):
-        colt_meta_setup(clsdict)
+        colt_modify_class_dict(clsdict, bases)
         return ABCMeta.__new__(cls, name, bases, clsdict)
 
     @property
--- plugin_example.py
+++ plugin_example.py
@@ -13,6 +13,7 @@ class Example(Colt):
 class QMFactory(PluginBase):
     """QMFactory"""
 
+
     _plugins_storage = '_software'
     _is_plugin_factory = True
 
@@ -22,6 +23,7 @@ class QMFactory(PluginBase):
 
     @classmethod
     def _generate_subquestions(cls, questions):
+        print("is that here ever called???")
         questions.generate_cases("software", {name: sampling.questions
                                               for name, sampling in cls._software.items()})
         questions.add_questions_to_block(Example.questions)
@@ -30,22 +32,27 @@ class QMFactory(PluginBase):
     def create_interface(cls):            
         questions = cls.generate_questions("qm", config='factory2.ini')
         answer = questions.ask('factory2.ini')
+        print(answer['software'].value)
+        print(cls._software)
         return cls.get_interface(answer['software'].value)
 
     @classmethod
     def get_interface(cls, name):
-        return cls._software.get(name, None)
+        return cls._software.get(name)
 
 
 class QMBase(QMFactory):
 
+    subquestions : 'inherited'
     _register_plugin = False
 
     def say_hallo(self):
         print("hallo")
 
+
 class QChemFactory(QMBase):
 
+    subquestions : 'inherited'
 
     _plugins_storage = '_software'
     _is_plugin_factory = True
@@ -57,8 +64,9 @@ class QChemFactory(QMBase):
 
     @classmethod
     def _generate_subquestions(cls, questions):
-        questions.generate_cases("software", {name: sampling.questions
-                                              for name, sampling in cls._software.items()})
+        print('wuhu...seems to work')
+        print(f"cls = {type(cls)}")
+        print(f"cls = {cls.__name__}")
 
 
 class QChemBase(QChemFactory):
@@ -67,12 +75,14 @@ class QChemBase(QChemFactory):
     def say_hallo(self):
         print("I am a qchem specialisation")
 
+
 class QChemTDDFT(QChemBase):
     _questions = """
         qchem = True :: bool
         basis = sto-3g
         functional = cam-b3lyp
     """
+
 
 class QChemADC(QChemBase):
     _questions = """
@@ -81,12 +91,14 @@ class QChemADC(QChemBase):
         functional = cam-b3lyp
     """
 
+
 class Turbomole(QMBase):
     _questions = """
         qchem = True :: bool
         basis = sto-3g
         functional = cam-b3lyp
     """
+
 
 class Gaussian(QMBase):
     _questions = """
@@ -96,7 +108,8 @@ class Gaussian(QMBase):
         functional = cam-b3lyp
     """
 
+
 print("ALL")
-print(QMFactory._software)
+print(QMFactory())
 print("QCHEM")
-print(QChemFactory._software)
+print(QChemFactory.create_interface())
