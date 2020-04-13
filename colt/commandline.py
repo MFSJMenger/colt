4b7be647ae34d852f08b786a72858aec6ad8973a
Maximilian Menger <menger.maximilian@gmail.com>
2020-04-13 13:25:13 +0200

    updated commandline

--- colt/commandline.py
+++ colt/commandline.py
@@ -1,31 +1,35 @@
 from .colt import Colt
 
 
+def _init(self, function):
+    self._function = function
+    self.__doc__ = self._function.__doc__
+
+
+def _call(self, *args, **kwargs):
+    # call with arguments
+    if any(len(value) != 0 for value in (args, kwargs)):
+        return self._function(*args, **kwargs)
+    # call from commandline
+    answers = self.from_commandline(self._description)
+    return self._function(**answers)
+
+
+def _from_config(cls, answers):
+    return answers
+
+
 class FromCommandline:
 
     def __init__(self, questions, description=None):
 
-        class Example(Colt):
-            _questions = questions
-            _description = description
-
-            def __init__(self, function):
-                self._function = function
-                self.__doc__ = self._function.__doc__
-
-            @classmethod
-            def from_config(cls, answers):
-                return answers
-
-            def __call__(self, *args, **kwargs):
-                # call with arguments
-                if any(len(value) != 0 for value in (args, kwargs)):
-                    return self._function(*args, **kwargs)
-                # call from commandline
-                answers = self.from_commandline(self._description)
-                return self._function(**answers)
-
-        self._cquest = Example
+        self._cls = type("CommandlineInterface", (Colt,), {
+            '_questions': questions,
+            '_description': description,
+            '__init__': _init,
+            'from_config': classmethod(_from_config),
+            '__call__': _call,
+            })
 
     def __call__(self, function):
-        return self._cquest(function)
+        return self._cls(function)
