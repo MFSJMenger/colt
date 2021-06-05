ce8206b9859f7926b1e1a4403bf8e5611819ef60
Maximilian Menger <menger.maximilian@gmail.com>
2021-06-05 12:52:57 +0200

    added switch to old cli parser

--- colt/colt.py
+++ colt/colt.py
@@ -195,7 +195,7 @@ class Colt(metaclass=ColtMeta):
                         "also from_questions depend on that!")
 
     @classmethod
-    def from_commandline(cls, *args, description=None, presets=None, logo=None, new_parser=True, **kwargs):
+    def from_commandline(cls, *args, description=None, presets=None, logo=None, argparse=False, **kwargs):
         """Initialize the class using input provided from the commandline
 
         Parameters
@@ -218,7 +218,7 @@ class Colt(metaclass=ColtMeta):
             anything that from_config returns. Intended to initalize the class
             so from_config should return an instance of the class.
         """
-        if new_parser is True:
+        if argparse is False:
             answers = get_config_from_commandline_parser(cls.questions, description=description,
                                                          presets=presets, logo=logo)
         else:
@@ -285,11 +285,11 @@ class CommandlineInterface(Colt):
         if any(len(value) != 0 for value in (args, kwargs)):
             return self.function(*args, **kwargs)
         # call from commandline
-        answers = self.from_commandline(description=self._description, logo=self.logo, new_parser=self.new_parser)
+        answers = self.from_commandline(description=self._description, logo=self.logo, argparse=self.use_argparse)
         return self.function(**answers)
 
 
-def from_commandline(questions, *, new_parser=True, logo=None, description=None):
+def from_commandline(questions, *, argparse=False, logo=None, description=None):
     """Decorate a function to call it using commandline arguments
 
     Parameters
@@ -317,7 +317,7 @@ def from_commandline(questions, *, new_parser=True, logo=None, description=None)
     def _wrapper(func):
         """Wrapper function to decorate the function with the CommandlineInterface class"""
         cls = type('CommandlineInterface', (CommandlineInterface,),
-                {'_questions': questions, '_description': description, 'new_parser': new_parser,
+                {'_questions': questions, '_description': description, 'use_argparse': argparse,
                  'logo': logo, 'description': description})
         return cls(func)
     # return the new colt class
